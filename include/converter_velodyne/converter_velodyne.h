c216a5dea921052863e7cf024e315f792ea7af65
Eilvrin <whitetigrrr@gmail.com>
2017-04-10 18:56:03 +0200

    Remove some parameters. Add dependences on msgs velodyne and velodyne16.

--- include/converter_velodyne/converter_velodyne.h
+++ include/converter_velodyne/converter_velodyne.h
@@ -5,9 +5,9 @@
 
 // ROS headers
 #include <ros/ros.h>
-#include <converter_velodyne/VelodynePacket.h>
-#include <converter_velodyne/VelodyneScan.h>
-#include <converter_velodyne/Velodyne16Packet.h>
+#include <velodyne_msgs/VelodynePacket.h>
+#include <velodyne_msgs/VelodyneScan.h>
+#include <velodyne16/VelodynePacket.h>
 
 
 class ConverterVelodyne
@@ -18,13 +18,11 @@ public:
   ConverterVelodyne(ros::NodeHandle node, ros::NodeHandle private_nh);
 
 private:
-  void convertScan(const converter_velodyne::VelodyneScan::ConstPtr &scanMsg);
+  void convertScan(const velodyne_msgs::VelodyneScan::ConstPtr &scanMsg);
   ros::Subscriber velodyne_scan_;
   ros::Publisher velodyne16_packet_;
 
   std::string frame_id_;   // Frame_id for output velodyne16 packets
-  bool velodyne_frame_id_; // Whether to use the initial velodyne frame for the velodyne16 packets
-  bool end_of_pkt_timestamp_; //whether or not for the velodyne the timestamp was set to the end of packet collection
 };
 
 #endif // CONVERTER_VELODYNE_H
